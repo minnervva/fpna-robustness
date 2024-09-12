ff0abe08c3fc58f2eb0a0b4db12e4c8296ede8a0
cculver <chris.cb.culver@gmail.com>
2024-09-12 16:38:52 +0100

    Added nonatomic linear

--- codes/test_pytorch_lightning/models/models.py
+++ codes/test_pytorch_lightning/models/models.py
@@ -1,6 +1,25 @@
 import torch
 import torch.nn as nn
 
+class AtomicLinear(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super(AtomicLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = torch.nn.Parameter(torch.randn(out_features, in_features))
+        self.bias = torch.nn.Parameter(torch.randn(out_features))
+
+    # x has dimension [batch_size, in_features]
+    def forward(self, x):
+        # return x.matmul(self.weight.t()) + self.bias
+        # percent_flipped = 0.5
+        # [0,1,2,3] + torch.randperm([4,5,6,7])
+        y=x[:, None, :] * self.weight
+        #with torch.no_grad():
+        indices = torch.randperm(self.in_features)
+        #print(indices)
+        prod = y[:,:,indices]
+        return prod.sum(dim=2) + self.bias[None, :]
 
 class MNISTModel(nn.Module):
     def __init__(self):
